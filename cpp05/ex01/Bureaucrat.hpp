ec6e6ae9f2e086295f374ee2e9316c66e70d3944
Ayoub Arab <ayarab@bess-f4r2s3.clusters.42paris.fr>
2025-04-21 20:53:47 +0200

    sss

---
+++ cpp05/ex01/Bureaucrat.hpp
@@ -0,0 +1,35 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+
+class Bureaucrat 
+{
+	private:
+		std::string Name;
+		int Grade;
+	public:
+		Bureaucrat(const std::string _name, int grade);
+		Bureaucrat(const Bureaucrat &Bureaucrat_cpy);
+		Bureaucrat &operator=(const Bureaucrat &Bureaucrat_aff);
+		~Bureaucrat(void);
+		std::string GetName(void);
+		int GetGrade(void);
+		void incrementGrade();
+		void decrementGrade();
+
+		class GradeTooHighException : public std::exception {
+			public:
+			const char* what() const throw();
+		};
+		
+		class GradeTooLowException : public std::exception {
+			public:
+			const char* what() const throw();
+		};
+		friend std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+	};
+	
+#endif
+
+
--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -6,32 +6,26 @@
 /*   By: ayarab <ayarab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:37:30 by ayarab            #+#    #+#             */
-/*   Updated: 2025/04/21 20:13:03 by ayarab           ###   ########.fr       */
+/*   Updated: 2025/04/21 18:43:52 by ayarab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include <cstdlib>
-#include <exception>
 #include <iostream>
 int main() 
 {
-    Bureaucrat test("ayoub", 150);
-    Bureaucrat valid("Wassim", 2);
+    Bureaucrat test("ayoub", 151);
     try{
-        std::cout << test << std::endl;
-        test.decrementGrade();
-        std::cout << test << std::endl;
-
-    } catch(std::exception &e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+        
     }
     try {
+        Bureaucrat valid("Wassim", 2);
         std::cout << valid << std::endl;
         valid.incrementGrade(); 
         std::cout << valid << std::endl;
         valid.incrementGrade(); 
-    } catch (std::exception &e) {
+    } catch (std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
     return 0;
