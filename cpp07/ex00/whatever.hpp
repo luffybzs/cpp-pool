a7f92d76660d9a486613446191859714d8e1f3f5
ARAB <ayoub.arab95@outlook.fr>
2025-05-05 00:51:08 +0200

    aaa

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,31 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+
+template  <typename T>
+void swap(T &a, T &b) 
+{
+	T buff;
+	
+	buff = a;
+	a = b;
+	b = buff;
+}
+
+template  <typename T>
+T const &min(T const &a, T const &b) 
+{
+	if (a < b)
+		return a;
+	return b;
+}
+
+template  <typename T>
+T const &max(T const &a, T const &b) 
+{
+	if (a > b)
+		return a;
+	return b;
+}
+
+#endif
